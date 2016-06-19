d7e561ed8eecd2618175d0d957aa12929fa9eadb
Tim Wu <tim7012@msn.com>
2016-06-20 02:05:18 +0800

    一對多 CRUD

---
+++ app/models/attendee.rb
@@ -0,0 +1,6 @@
+class Attendee < ActiveRecord::Base
+
+  validates_presence_of :name
+
+  belongs_to :event
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :events do
-
+    resources :attendees, :controller => "event_attendees"
   end
 
   root 'events#index'
